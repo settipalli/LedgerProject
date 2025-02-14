22de51b419bcfb4b1a6e20c7280dd9fa804cb1c7
Santhosh Settipalli <santhosh.settipalli@gmail.com>
2025-02-14 22:17:21 +0000

    Add Account and Transaction domain model.

--- models/account.go
+++ models/account.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Account struct {
+    ID             string    `json:"id"`
+    Name           string    `json:"name"`
+    Balance        Money     `json:"balance"`
+    Type           string    `json:"type"`
+    Currency       string    `json:"currency"`
+    CreateDateTime time.Time `json:"datetime"`
+}
--- models/transaction.go
+++ models/transaction.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Transaction struct {
+    ID            string    `json:"id"`
+    DateTime      time.Time `json:"datetime"`
+    Description   string    `json:"description"`
+    DebitAccount  string    `json:"debit_account"`
+    CreditAccount string    `json:"credit_account"`
+    Money         Money     `json:"money"`
+}
